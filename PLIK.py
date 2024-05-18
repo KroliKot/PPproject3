7771acec6d4cbf018269459755518bc678355b63
Septey <arek.lewandowski727@wp.pl>
2024-05-18 14:33:26 +0200

    50%

--- PLIK.py
+++ PLIK.py
@@ -1,2 +1,13 @@
+import wave
+import struct
 from build.Debug.Project3 import *
-print(TEST())
+
+chart=input("1-sin 2-cos 3-squarewave 4-sawtooth: ")
+SIGNAL(int (chart))
+
+with wave.open('murloc.wav') as f:
+    data = f.readframes(f.getnframes())
+    duration = f.getnframes()/f.getframerate()
+    audio = struct.unpack('{n}h'.format(n=f.getnframes()*f.getnchannels()), data)
+
+AUDIO(audio,duration)
--- mian.cpp
+++ mian.cpp
@@ -7,16 +7,32 @@ using namespace std;
 using namespace matplot;
 using namespace pybind11;
 
-void TEST()
+void SIGNAL(int chart)
 {
-vector<double> x = linspace(0, 2 * pi, 1000);
-vector<double> y = transform(x, [](auto x) { return sin(x); });
+    vector<double> x = linspace(0, 10 * pi, 10000);
+    vector<double> y;
+if (chart == 1)  y = transform(x, [](auto x) { return sin(x); });
+if (chart == 2)  y = transform(x, [](auto x) { return cos(x); });
+if (chart == 3)  y = transform(x, [](auto x) { return (sin(x)>0) ? 1: -1; });
+if (chart == 4) 
+{
+    x=linspace(0, 8, 10000);
+    y=transform(x, [](auto x) { return x-floor(x); });
+}
 plot(x,y);
 show();
+}
+
+void AUDIO(vector<double> audio, double duration)
+{
+    vector<double> x = linspace(0, duration, audio.size());
+    plot(x,audio);
+    show();
 }
 
 
 PYBIND11_MODULE(Project3, yes)
 {
-    yes.def("TEST", &TEST, "TESTUJE");
+    yes.def("SIGNAL", &SIGNAL, "generates signal");
+    yes.def("AUDIO", &AUDIO, "generates audio");
 }
